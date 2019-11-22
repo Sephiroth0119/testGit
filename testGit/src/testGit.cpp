9d213f6916ec5f3c1eb84bd688870b807ef4abf9
Sephiroth0119 <524347655@qq.com>
2019-11-22 09:17:48 +0800

    Create Github master branch

---
+++ testGit/src/testGit.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : testGit.cpp
+// Author      : sjh
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	printf("Hello Github!");
+
+	return 0;
+}
